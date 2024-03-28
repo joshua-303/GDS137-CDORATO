e4f8e94694629f949feaee70d7da9cfeac83fa36
Christian Dorato <christian.a.dorato@gmail.com>
2024-03-28 19:44:11 -0400

    Level 1 boss challenge

--- level1/js/class.js
+++ level1/js/class.js
@@ -1,13 +1,22 @@
 class Ball {
     constructor() {
+        //initializes the ball in the center of the canvas
         this.x = canvas.width/2;
         this.y = canvas.height/2;
 
         this.width = 100;
         this.height = 100;
 
-        this.vx = 2;
-        this.vy = 2;
+        //horizontal and vertical speeds
+        this.vx = 1;
+        this.vy = 1;
+
+        //rate of acceleration (when colliding with walls)
+        this.xAccel = 1;
+        this.yAccel = 1;
+
+        //speed cap
+        this.sCap = 10;
 
         this.color = "#ff0000";
 
--- level1/js/game.js
+++ level1/js/game.js
@@ -1,26 +1,48 @@
 var canvas = document.getElementById("canvas");
 var context = canvas.getContext("2d");
-var timer;
 var interval = 1000/60;
+var collCount = 0;
 
 var ball = new Ball();
 
-timer = setInterval(animate, interval);
+var timer = setInterval(animate, interval);
 
 ball.draw();
 
 function animate() {
     context.clearRect(0,0,canvas.width, canvas.height);
 
+    //caps vx and vy to the speed cap, sCap (set to 10)
+    if (ball.vx == ball.sCap && ball.xAccel == 1 || ball.vx == -ball.sCap && ball.xAccel == -1) {
+        ball.xAccel = 0;
+    }
+
+    if (ball.vy == ball.sCap && ball.yAccel == 1 || ball.vy == -ball.sCap && ball.yAccel == -1) {
+        ball.yAccel = 0;
+    }
+
     ball.move();
 
-    if(ball.x > canvas.width - ball.width/2 || ball.x < 0 + ball.width/2) {
+    if(ball.x >= canvas.width - ball.width/2 || ball.x <= 0 + ball.width/2) {
+        ball.vx+=ball.xAccel;
         ball.vx = -ball.vx;
+        ball.xAccel = -ball.xAccel;
+        collCount++;
+        //console.log("VX: " + ball.vx);
+        //console.log("XACCEL: " + ball.xAccel);
     }
 
-    if(ball.y > canvas.height - ball.height/2 || ball.y < 0 + ball.height/2) {
+    if(ball.y >= canvas.height - ball.height/2 || ball.y <= 0 + ball.height/2) {
+        ball.vy+=ball.yAccel;
         ball.vy = -ball.vy
+        ball.yAccel = -ball.yAccel;
+        collCount++;
+        //console.log("VY: " + ball.vy);
+        //console.log("YACCEL: " + ball.yAccel)
     }
+
+    //prints collCount to h1 "test" in the html file
+    document.getElementById("test").innerHTML = collCount + " HITS";
 
     ball.draw();
 }
