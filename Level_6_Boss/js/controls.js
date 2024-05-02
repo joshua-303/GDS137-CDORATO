16ffbfef271aa23deb714865b0031248302d01b1
unknown <cadorato@email.neit.edu>
2024-05-02 14:05:07 -0400

    6.3

---
+++ Level_6_Boss/js/controls.js
@@ -0,0 +1,48 @@
+var a = false;
+var d = false;
+var w = false;
+var s = false;
+var newKey;
+var lastKeyPressed;
+
+document.addEventListener("keydown", press);
+document.addEventListener("keyup", release);
+
+function press(e) {
+    console.log(e.keyCode);
+
+    if(e.keyCode == 65) {
+        a = true;
+    }
+    if(e.keyCode == 68) {
+        d = true;
+    }
+    if(e.keyCode == 87) {
+        w = true;
+    }
+    if(e.keyCode == 83) {
+        s = true;
+    }
+
+    keyHasReleased = false;
+    newKey = e.keyCode;
+    //console.log(newKey + ' ' + lastKeyPressed);
+}
+
+function release(e) {
+    if(e.keyCode == 65) {
+        a = false;
+    }
+    if(e.keyCode == 68) {
+        d = false;
+    }
+    if(e.keyCode == 87) {
+        w = false;
+    }
+    if(e.keyCode == 83) {
+        s = false;
+    }
+
+    keyHasReleased = true;
+    lastKeyPressed = e.keyCode;
+}
